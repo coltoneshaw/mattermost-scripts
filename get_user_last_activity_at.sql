de5815f4e2ccb4bd5cdec42deef0e320fb383154
Colton Shaw <46071821+coltoneshaw@users.noreply.github.com>
2020-09-21 09:24:12 -0400

    Update get_user_last_activity_at.sql
    legnth to length

--- get_user_last_activity_at.sql
+++ get_user_last_activity_at.sql
@@ -2,7 +2,7 @@
 # The following SQL query retrieves a list of all users
 # and their last session activity at date and time.
 # Important Note: If the time that the the user was last 
-# active at exceeded the configured session legnth in
+# active at exceeded the configured session length in
 # days, or the user has never logged in, the
 # LastActivityAt field will be null.
 # Tested on MySQL 5.7.24
